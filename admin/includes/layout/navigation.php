0357216f9c41b9962c73d5e02d9f1b601296155a
devmobin <mobinheydari114@gmail.com>
2020-07-15 23:40:29 +0430

    admin navigation links adjustments

--- admin/includes/layout/navigation.php
+++ admin/includes/layout/navigation.php
@@ -7,7 +7,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="index.php">SB Admin</a>
+        <a class="navbar-brand" href="index.php">CMS Admin</a>
     </div>
     <!-- Top Menu Items -->
     <ul class="nav navbar-right top-nav">
@@ -122,23 +122,25 @@
                 <a href="index.php"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
             </li>
             <li>
-                <a href="charts.html"><i class="fa fa-fw fa-bar-chart-o"></i> Charts</a>
+                <a href="javascript:;" data-toggle="collapse" data-target="#posts-dropdown"><i class="fa fa-fw fa-archive"></i> Posts <i class="fa fa-fw fa-caret-down"></i></a>
+                <ul id="posts-dropdown" class="collapse">
+                    <li>
+                        <a href="#">View All Posts</a>
+                    </li>
+                    <li>
+                        <a href="#">Add Posts</a>
+                    </li>
+                </ul>
             </li>
             <li>
-                <a href="tables.html"><i class="fa fa-fw fa-table"></i> Tables</a>
+                <a href="bootstrap-grid.html"><i class="fa fa-fw fa-list"></i> Categories</a>
             </li>
             <li>
-                <a href="forms.html"><i class="fa fa-fw fa-edit"></i> Forms</a>
+                <a href="blank-page.html"><i class="fa fa-fw fa-comments"></i> Comments</a>
             </li>
             <li>
-                <a href="bootstrap-elements.html"><i class="fa fa-fw fa-desktop"></i> Bootstrap Elements</a>
-            </li>
-            <li>
-                <a href="bootstrap-grid.html"><i class="fa fa-fw fa-wrench"></i> Bootstrap Grid</a>
-            </li>
-            <li>
-                <a href="javascript:;" data-toggle="collapse" data-target="#demo"><i class="fa fa-fw fa-arrows-v"></i> Dropdown <i class="fa fa-fw fa-caret-down"></i></a>
-                <ul id="demo" class="collapse">
+                <a href="javascript:;" data-toggle="collapse" data-target="#users-dropdown"><i class="fa fa-fw fa-users"></i> Users <i class="fa fa-fw fa-caret-down"></i></a>
+                <ul id="users-dropdown" class="collapse">
                     <li>
                         <a href="#">Dropdown Item</a>
                     </li>
@@ -147,11 +149,8 @@
                     </li>
                 </ul>
             </li>
-            <li class="active">
-                <a href="blank-page.html"><i class="fa fa-fw fa-file"></i> Blank Page</a>
-            </li>
             <li>
-                <a href="index-rtl.html"><i class="fa fa-fw fa-dashboard"></i> RTL Dashboard</a>
+                <a href="blank-page.html"><i class="fa fa-fw fa-user"></i> Profile</a>
             </li>
         </ul>
     </div>
