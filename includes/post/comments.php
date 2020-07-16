c5008b934f1b86645b817a238c6ab871847918a4
devmobin <mobinheydari114@gmail.com>
2020-07-17 01:48:40 +0430

    new comment form in the post page

--- includes/post/comments.php
+++ includes/post/comments.php
@@ -1,3 +1,27 @@
+<!-- Comments Form -->
+<div class="well">
+    <h4>Leave a Comment:</h4>
+    <form action="#" method="post" role="form">
+        <div class="form-group">
+            <label for="author">Author</label>
+            <input type="text" name="author" class="form-control" name="comment_author">
+        </div>
+
+        <div class="form-group">
+            <label for="email">Email</label>
+            <input type="email" name="email" class="form-control" name="comment_email">
+        </div>
+
+        <div class="form-group">
+            <label for="content">Your Comment</label>
+            <textarea name="content" class="form-control" rows="3"></textarea>
+        </div>
+        <button type="submit" name="create_comment" class="btn btn-primary">Submit</button>
+    </form>
+</div>
+
+<hr>
+
 <!-- Posted Comments -->
 
 <!-- Comment -->
