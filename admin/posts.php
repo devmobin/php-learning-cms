62f6fc0c725ff71040ea6dda93866db3be880ed4
devmobin <mobinheydari114@gmail.com>
2020-07-16 19:09:48 +0430

    added posts page template

---
+++ admin/posts.php
@@ -0,0 +1,25 @@
+<?php include 'includes/layout/header.php'; ?>
+
+    <div id="wrapper">
+
+        <!-- Navigation -->
+        <?php include 'includes/layout/navigation.php'; ?>
+
+        <div id="page-wrapper">
+            <div class="container-fluid">
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                            Welcome admin
+                            <small>Author</small>
+                        </h1>
+<!--                        TODO-->
+                    </div>
+                </div> <!-- /.row -->
+            </div> <!-- /.container-fluid -->
+        </div> <!-- /#page-wrapper -->
+
+    </div><!-- /#wrapper -->
+
+<?php include 'includes/layout/footer.php'; ?>
