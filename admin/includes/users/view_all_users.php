69994fbf7d2a2278a9cde5ec0ffa26caba5edea1
devmobin <mobinheydari114@gmail.com>
2020-07-17 20:45:07 +0430

    edit user role in admin page

--- admin/includes/users/view_all_users.php
+++ admin/includes/users/view_all_users.php
@@ -24,8 +24,8 @@
                 echo "<td>{$row['email']}</td>";
                 echo "<td>{$row['role']}</td>";
                 echo "<td><div class='btn-group btn-group-vertical'>";
-                echo "<a class='btn btn-primary btn-sm' href=''>Approve</a>";
-                echo "<a class='btn btn-warning btn-sm' href=''>Deny</a>";
+                echo "<a class='btn btn-primary btn-sm' href='users.php?role_admin={$row['id']}'>Admin</a>";
+                echo "<a class='btn btn-warning btn-sm' href='users.php?role_sub={$row['id']}'>Subscriber</a>";
                 echo "<a class='btn btn-danger btn-sm' href='users.php?delete={$row['id']}'>Delete</a>";
                 echo "</div></td>";
                 echo "</tr>";
@@ -33,6 +33,28 @@
         ?>
     </tbody>
 </table>
+
+<?php // CHANGE ROLE TO ADMIN
+    if (isset($_GET['role_admin'])) {
+        $id = $_GET['role_admin'];
+
+        $query = "UPDATE users SET role = 'admin' WHERE id = {$id}";
+        $sql_query = mysqli_query($connection, $query);
+        confirm_query($sql_query);
+        header('Location: users.php');
+    }
+?>
+
+<?php // CHANGE ROLE TO SUBSCRIBER
+    if (isset($_GET['role_sub'])) {
+        $id = $_GET['role_sub'];
+
+        $query = "UPDATE users SET role = 'subscriber' WHERE id = {$id}";
+        $sql_query = mysqli_query($connection, $query);
+        confirm_query($sql_query);
+        header('Location: users.php');
+    }
+?>
 
 <?php // DELETE USER
     if (isset($_GET['delete'])) {
